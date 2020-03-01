53cff53824ecf0ed3877e40d9dc45458eae9557a
r44083 <r44083.r@gmail.com>
2020-03-01 11:06:30 +0200

    Use table-parser npm module instead of custom one. Refactoring

--- src/index.js
+++ src/index.js
@@ -1,52 +1,48 @@
-import { parse_ps } from './parse_ps';
+var TableParser = require('table-parser');
 
-var file;
-
-//document.addEventListener("DOMContentLoaded", main);
-
-//function main()
 window.onload = function()
 {
-    //await wdr();
-    //testing();
-    
     const file_input = document.getElementById('file-input');
     const analize_button = this.document.getElementById('analyze-button');
     
-    
-    
-    file_input.addEventListener('change', on_choose_file);
-    analize_button.addEventListener('click', on_click_analyze);
-    /*{
-        e.preventDefault();
-
-        alert("mydebug");
-    });*/
-}
-
-function on_choose_file()
-{
-    console.log('Selected file:', this.files[0]);
-    
-    var reader = new FileReader();
-    reader.addEventListener('load',
-    function (e)
+    analize_button.addEventListener('click', function(e)
     {
-        file = e.target.result;
+        if(!file_input.files[0])
+        {
+            const err_msg = 'No file is loaded';
+            alert(err_msg);
+            console.error(err_msg);
+            return;
+        }
+        
+        const reader = new FileReader();
+        reader.addEventListener('load', function (e)
+        {
+            const header = e.target.result.match(/^.*[\r]?\n/m);
+            let splitted = e.target.result.split(header);
+            if(splitted.length <= 2)
+            {
+                const err_msg = 'Incorrect format. Does file have \"S   UID   PID ...\" header?';
+                alert(err_msg);
+                console.error(err_msg);
+                return;
+            }
+            splitted.shift(); // Remove first empty element
+            
+            let parsed = [];
+            splitted.forEach(function(elem, index, arr) {
+                let ps_parsed = TableParser.parse(header + elem);
+                if(ps_parsed.length === 0)
+                    console.warn('Incorrect format in line', index);
+                else
+                    parsed.push(ps_parsed);
+                
+                console.log('TODO: Progress bar + %');
+            });
+            
+            console.log('Parsed:\n', parsed);
+        });
+        
+        reader.readAsText(file_input.files[0]);
     });
-    
-    reader.readAsText(this.files[0]);
-}
-
-function on_click_analyze()
-{
-    if(!file)
-    {
-        var err_msg = "No file is loaded";
-        alert(err_msg);
-        console.error(err_msg);
-        return;
-    }
-    
-    console.log("Parsed:\n", parse_ps(file));
 }
